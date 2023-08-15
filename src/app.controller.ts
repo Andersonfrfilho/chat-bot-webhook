de417d20a7f5139c22915cafe32886ef8452f003
Anderson <anderson.filho@catenait.com.br>
2023-08-15 10:26:11 -0300

    chore: correct response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Res, HttpStatus, Req } from '@nestjs/common';
+import { Controller, Post, Res, HttpStatus, Req } from '@nestjs/common';
 import { Response, Request } from 'express';
 
 @Controller()
@@ -6,6 +6,6 @@ export class AppController {
   @Post('/webhook')
   getHello(@Req() req: Request, @Res() res: Response) {
     const body = req.body;
-    req.res.status(HttpStatus.OK).json(body);
+    res.status(HttpStatus.OK).json(body);
   }
 }
