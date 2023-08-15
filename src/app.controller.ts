02eb9f7f5db3c23b08b6b824c1108a28b38d954a
Anderson <anderson.filho@catenait.com.br>
2023-08-15 11:04:54 -0300

    chore: add logger stringfy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   @Post('/webhook')
   getHello(@Req() req: Request, @Res() res: Response) {
     const body = req.body;
-    console.log(body);
+    console.log(JSON.stringify(body, null, 2));
     res.status(HttpStatus.OK).json(body);
   }
 }
